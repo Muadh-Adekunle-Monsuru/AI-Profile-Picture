76a42f41745d37c00b7fc115fd5b262dbbd6ed68
Muadh Adekunle Monsuru <muadh117a@gmail.com>
2024-08-12 19:53:30 +0100

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,101 @@
-import Image from "next/image";
+'use client';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
+import { Skeleton } from '@/components/ui/skeleton';
+import { Textarea } from '@/components/ui/textarea';
+import * as fal from '@fal-ai/serverless-client';
+import { useState } from 'react';
+import { Loader, ArrowDownToLine } from 'lucide-react';
+fal.config({
+	proxyUrl: '/api/fal/proxy',
+});
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [prompt, setPrompt] = useState('');
+	const [image, setImage] = useState(
+		'https://images.unsplash.com/photo-1701014159251-f86a81a6fe13?q=80&w=1926&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+	);
+	const [loading, setLoading] = useState(false);
+	const handleSubmit = async () => {
+		if (!prompt) return;
+		setLoading(true);
+		const result: any = await fal.subscribe('110602490-lora', {
+			input: {
+				prompt,
+				model_name: 'stabilityai/stable-diffusion-xl-base-1.0',
+				image_size: 'square_hd',
+			},
+			pollInterval: 5000,
+			logs: true,
+			onQueueUpdate(update) {
+				console.log('queue update', update);
+			},
+		});
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+		console.log(result);
+		setImage(result.images[0].url);
+		setLoading(false);
+	};
+	const handleDownload = async () => {
+		try {
+			const response = await fetch(image);
+			const blob = await response.blob();
+			const url = window.URL.createObjectURL(blob);
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+			// Create a temporary anchor element to initiate the download
+			const link = document.createElement('a');
+			link.href = url;
+			link.download = 'downloaded-image.jpg'; // Default filename
+			document.body.appendChild(link);
+			link.click();
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+			// Clean up
+			document.body.removeChild(link);
+			window.URL.revokeObjectURL(url);
+		} catch (error) {
+			console.error('Failed to download image', error);
+		}
+	};
+	return (
+		<main className='flex min-h-screen flex-col items-center justify-center gap-5  backdrop-blur-sm bg-black/20 '>
+			<h1 className='text-center font-semibold text-4xl  blur-[0.3px] drop-shadow-sm'>
+				AI Profile Picture
+			</h1>
+			<p className='text-xs'>
+				Powered By{' '}
+				<a href='https://stability.ai/' className='font-medium'>
+					Stable Diffusion
+				</a>
+			</p>
+			<div className='size-72 rounded-full border-2 drop-shadow-sm bg-slate-100 flex items-center justify-center overflow-hidden relative'>
+				{loading && (
+					<div className=' flex items-center justify-center flex-col'>
+						<Loader className='size-5 animate-spin' />
+						<p className='animate-pulse'>Generating... </p>
+					</div>
+				)}
+				{image && !loading && (
+					<img src={image} className='w-full h-full object-cover' />
+				)}
+			</div>
+			{image && !loading && (
+				<Button variant={'ghost'} onClick={handleDownload}>
+					Download
+					<ArrowDownToLine className='size-4 inline ml-2' />
+				</Button>
+			)}
+			<div className='px-5 flex flex-col gap-2 md:flex-row w-full max-w-sm items-center space-x-2'>
+				<Input
+					value={prompt}
+					onChange={(e) => setPrompt(e.target.value)}
+					placeholder='Type your desciption here.'
+					id='message'
+					className='bg-white/50'
+				/>
+				<Button onClick={() => handleSubmit()} type='submit' disabled={loading}>
+					{loading ? 'Generating...' : 'Generate'}
+				</Button>
+			</div>
+		</main>
+	);
 }
